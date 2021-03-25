2a17bac72bf81eede97b492b30beb6639e86ae25
vinnu770 <vinnureddy05@gmail.com>
2021-03-25 08:58:03 +0000

    done

--- Program-9.c
+++ Program-9.c
@@ -4,14 +4,14 @@
 
 int main()
 {
-    float x1,x2,y1,y2,slope,; // correct the code
+    float x1,x2,y1,y2,slope,midX,midY; // correct the code
     printf(" Enter the X Coordinate and Y coordinate of Endpoint 1 and Endpoint 2: ");
-    scanf("%f%f%f%f");   // correct the code
+    scanf("%f%f%f%f",&x1, &y1, &x2, &y2);   // correct the code
     //printf(" The Endpoints of a Line are : (%.2f,%.2f) and (%.2f,%.2f)",x1,y1,x2,y2);
     slope=(y2-y1)/(x2-x1);
     midX=(x1+x2)/2;
     midY=(y1+y2)/2;
     printf("Slope : %.2f ",slope);
-    printf("Midpoint : (%.,%.)",midX,midY);  // correct the code to print the output with numbers after the decimal point
+    printf("Midpoint : (%.2f,%.2f)",midX,midY);  // correct the code to print the output with numbers after the decimal point
     return 0;
 }
